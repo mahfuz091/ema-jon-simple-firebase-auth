543056584bc9478d1a19249e88c18b52fbebbc23
mahfuz091 <engmahfuz14@gmail.com>
2023-04-17 01:42:43 +0600

    added login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import './Header.css';
-import logo from '../../images/Logo.svg';
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react";
+import "./Header.css";
+import logo from "../../images/Logo.svg";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../AuthProviders/AuthProviders";
 
 const Header = () => {
-    return (
-        <nav className='header'>
-            <img src={logo} alt="" />
-            <div>
-                <Link to="/">Shop</Link>
-                <Link to="/orders">Orders</Link>
-                <Link to="/inventory">Inventory</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/signup">Signup</Link>
-            </div>
-        </nav>
-    );
+  const { user, logOut } = useContext(AuthContext);
+  console.log(user);
+  const handleLogout = () => {
+    logOut()
+      .then((result) => {})
+      .catch((error) => console.error(error));
+  };
+  return (
+    <nav className='header'>
+      <img src={logo} alt='' />
+      <div>
+        <Link to='/'>Shop</Link>
+        <Link to='/orders'>Orders</Link>
+        <Link to='/inventory'>Inventory</Link>
+        <Link to='/login'>Login</Link>
+        <Link to='/signup'>Signup</Link>
+        {user && (
+          <span className='text-white'>
+            Welcome {user.email}{" "}
+            <button onClick={handleLogout}> Log out</button>
+          </span>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,61 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
+import "./Login.css";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../AuthProviders/AuthProviders";
 
 const Login = () => {
-    return (
-        <div>
-            <h2>Login coming sooon</h2>
+  const [error, setError] = useState("");
+  const { signIn } = useContext(AuthContext);
+
+  const handleLogIn = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    setError("");
+    signIn(email, password)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        form.reset();
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+  return (
+    <div className='form-container'>
+      <h2 className='form-title'>Login</h2>
+      <form onSubmit={handleLogIn}>
+        <div className='form-control'>
+          <label htmlFor='email'>Email</label>
+          <input type='email' name='email' id='' required />
         </div>
-    );
+        <div className='form-control'>
+          <label htmlFor='password'>Password</label>
+          <input
+            // type={show ? "text" : "password"}
+            type='password'
+            name='password'
+            id=''
+            required
+          />
+          {/* <p onClick={() => setShow(!show)}>
+            <small>
+              {show ? <span>Hide Password</span> : <span>Show Password</span>}
+            </small>
+          </p> */}
+        </div>
+        <input className='btn-submit' type='submit' value='Login' />
+      </form>
+      <p>
+        <small>
+          New to Ema-john? <Link to='/signup'>Create New Account</Link>
+        </small>
+      </p>
+    </div>
+  );
 };
 
 export default Login;
