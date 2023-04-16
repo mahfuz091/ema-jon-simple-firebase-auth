dac9ff0ebaa04bdbfe4162a52856cf240afda282
mahfuz091 <engmahfuz14@gmail.com>
2023-04-16 22:34:16 +0600

    'signup'

---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp = () => {
+    return (
+        <div>
+            Please Signup
+        </div>
+    );
+};
+
+export default SignUp;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDKBUqQsdxNq7ZccVWZaUnsu0VIDZ8kdcw",
+    authDomain: "ema-john-firebase-auth-d3295.firebaseapp.com",
+    projectId: "ema-john-firebase-auth-d3295",
+    storageBucket: "ema-john-firebase-auth-d3295.appspot.com",
+    messagingSenderId: "650500258332",
+    appId: "1:650500258332:web:6b2cd7d6b119e95bb3bdd9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
